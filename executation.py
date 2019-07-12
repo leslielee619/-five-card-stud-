f0008f2d2ae407e409cab90f64c0d4235789df1b
leslielee619 <1468269411@qq.com>
2019-07-12 14:59:05 +0800

    Update executation.py
    执行这个文件后，会得到统计出来的直方图

--- executation.py
+++ executation.py
@@ -8,10 +8,10 @@ Created on Thu Jul 11 23:11:23 2019
 写的也比较笨，应该将列表都写入json，然后从json里面读取即可，这样便不会占用内存
 这个模块就是运行后得到各个牌型的统计结果，并且得到一个直方图。
 """
-import hands_census
+import census_hist
 
 
-frequencies1,frequencies2 = hands_census.census(1e6)
-hands_census.hist(frequencies1,1e6)
-hands_census.hist(frequencies2,1e6)
+frequencies1,frequencies2 = census_hist.census(1e6)
+census_hist.hist(frequencies1,1e6)
+census_hist.hist(frequencies2,1e6)
 
